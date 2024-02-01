cb6439af7d4aa29be4ce8ecd472d2cc2d31c7486
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-02-01 15:00:31 -0300

    transaction_list.dart: use list view instead of column to optimize app

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -9,46 +9,51 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: transactions
-          .map((e) => Card(
-                child: Row(
-                  children: [
-                    Container(
-                      margin: const EdgeInsets.symmetric(
-                          horizontal: 15, vertical: 10),
-                      decoration: BoxDecoration(
-                          border: Border.all(
+    return SizedBox(
+      height: 300,
+      child: ListView.builder(
+        itemCount: transactions.length,
+        itemBuilder: (ctx, index) {
+          final e = transactions[index];
+          return Card(
+            child: Row(
+              children: [
+                Container(
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                    color: Colors.purple,
+                    width: 2,
+                  )),
+                  padding: const EdgeInsets.all(10),
+                  child: Text(
+                    'R\$ ${e.value.toStringAsFixed(2)}',
+                    style: const TextStyle(
                         color: Colors.purple,
-                        width: 2,
-                      )),
-                      padding: const EdgeInsets.all(10),
-                      child: Text(
-                        'R\$ ${e.value.toStringAsFixed(2)}',
-                        style: const TextStyle(
-                            color: Colors.purple,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20.0),
-                      ),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.0),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      e.title,
+                      style: const TextStyle(
+                          fontSize: 16.0, fontWeight: FontWeight.bold),
                     ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          e.title,
-                          style: const TextStyle(
-                              fontSize: 16.0, fontWeight: FontWeight.bold),
-                        ),
-                        Text(
-                          DateFormat('dd-MM-yyyy').format(e.date),
-                          style: TextStyle(color: Colors.grey.shade400),
-                        )
-                      ],
+                    Text(
+                      DateFormat('dd-MM-yyyy').format(e.date),
+                      style: TextStyle(color: Colors.grey.shade400),
                     )
                   ],
-                ),
-              ))
-          .toList(),
+                )
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
