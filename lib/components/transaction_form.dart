7412ea0b0ff10fe303c92983b9c0cd7e44dcfb9a
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-02-01 14:37:40 -0300

    transaction_form.dart: added func param to add new transactions to list

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -4,7 +4,9 @@ class TransactionForm extends StatelessWidget {
   final titleController = TextEditingController();
   final valueController = TextEditingController();
 
-  TransactionForm({super.key});
+  final void Function(String, double) onSubmit;
+
+  TransactionForm({super.key, required this.onSubmit});
 
   @override
   Widget build(BuildContext context) {
@@ -30,8 +32,11 @@ class TransactionForm extends StatelessWidget {
                 children: [
                   TextButton(
                       onPressed: () {
-                        print(titleController.text);
-                        print(valueController.text);
+                        final title = titleController.text;
+                        final value =
+                            double.tryParse(valueController.text) ?? 0.0;
+
+                        onSubmit(title, value);
                       },
                       child: const Text(
                         'Nova Transação',
