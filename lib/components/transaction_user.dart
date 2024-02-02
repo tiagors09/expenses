65c33fecce69a0aeca3491fd6a798586730e87bc
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-02-02 10:49:56 -0300

    transaction_user.dart: cleaned file

--- lib/components/transaction_user.dart
+++ lib/components/transaction_user.dart
@@ -13,48 +13,8 @@ class TransactionUser extends StatefulWidget {
 }
 
 class _TransactionUserState extends State<TransactionUser> {
-  final _transactions = [
-    Transaction(
-      id: '1',
-      title: 'Compra de alimentos',
-      value: 50.0,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: '2',
-      title: 'Pagamento de conta',
-      value: 100.0,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: '3',
-      title: 'Gasolina',
-      value: 30.0,
-      date: DateTime.now(),
-    ),
-  ];
-
-  void _addTransaction(String title, double value) {
-    final newTransaction = Transaction(
-        id: Random().nextDouble().toString(),
-        title: title,
-        value: value,
-        date: DateTime.now());
-
-    setState(() {
-      _transactions.add(newTransaction);
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        TransactionForm(
-          onSubmit: _addTransaction,
-        ),
-        TransactionList(transactions: _transactions),
-      ],
-    );
+    return const Placeholder();
   }
 }
