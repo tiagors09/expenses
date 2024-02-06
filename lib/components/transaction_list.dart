4ce0d3faa02bf3f0893d44a3af4208c0246a38b6
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-02-06 20:09:22 -0300

    transaction_list.dart: added a conditional rendering when transaction list is empty

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -11,49 +11,68 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: 300,
-      child: ListView.builder(
-        itemCount: transactions.length,
-        itemBuilder: (ctx, index) {
-          final e = transactions[index];
-          return Card(
-            child: Row(
+      child: transactions.isEmpty
+          ? Column(
               children: [
-                Container(
-                  margin:
-                      const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                  decoration: BoxDecoration(
-                      border: Border.all(
-                    color: Colors.purple,
-                    width: 2,
-                  )),
-                  padding: const EdgeInsets.all(10),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 0, 0, 20.0),
                   child: Text(
-                    'R\$ ${e.value.toStringAsFixed(2)}',
-                    style: const TextStyle(
-                        color: Colors.purple,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20.0),
+                    'Nenhuma Transação Cadastrada!',
+                    style: Theme.of(context).textTheme.titleMedium,
                   ),
                 ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      e.title,
-                      style: const TextStyle(
-                          fontSize: 16.0, fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      DateFormat('dd-MM-yyyy').format(e.date),
-                      style: TextStyle(color: Colors.grey.shade400),
-                    )
-                  ],
-                )
+                SizedBox(
+                  height: 200,
+                  child: Image.asset(
+                    'images/waiting.png',
+                    fit: BoxFit.cover,
+                  ),
+                ),
               ],
+            )
+          : ListView.builder(
+              itemCount: transactions.length,
+              itemBuilder: (ctx, index) {
+                final e = transactions[index];
+                return Card(
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.symmetric(
+                            horizontal: 15, vertical: 10),
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                          color: Colors.purple,
+                          width: 2,
+                        )),
+                        padding: const EdgeInsets.all(10),
+                        child: Text(
+                          'R\$ ${e.value.toStringAsFixed(2)}',
+                          style: const TextStyle(
+                              color: Colors.purple,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20.0),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            e.title,
+                            style: const TextStyle(
+                                fontSize: 16.0, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            DateFormat('dd-MM-yyyy').format(e.date),
+                            style: TextStyle(color: Colors.grey.shade400),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              },
             ),
-          );
-        },
-      ),
     );
   }
 }
