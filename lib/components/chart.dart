8e532bdd4584f8b5ccb6db08676656d5a425af27
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-02-16 10:28:11 -0300

    chart.dart: reversing grouped transactions

--- lib/components/chart.dart
+++ lib/components/chart.dart
@@ -25,7 +25,7 @@ class Chart extends StatelessWidget {
       }
 
       return {'day': DateFormat.E().format(weekDay)[0], 'value': totalSum};
-    });
+    }).reversed.toList();
   }
 
   double get _weekTotalValue {
