d654f93946c2ea1dd48b1fea5926f4fd23c7f950
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-02-22 10:03:37 -0300

    transiction_list.dart: removed SizedBox

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -14,61 +14,57 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 300,
-      child: transactions.isEmpty
-          ? Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 0, 0, 20.0),
-                  child: Text(
-                    'Nenhuma Transação Cadastrada!',
-                    style: Theme.of(context).textTheme.titleMedium,
-                  ),
+    return transactions.isEmpty
+        ? Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 0, 20.0),
+                child: Text(
+                  'Nenhuma Transação Cadastrada!',
+                  style: Theme.of(context).textTheme.titleMedium,
                 ),
-                SizedBox(
-                  height: 200,
-                  child: Image.asset(
-                    'images/waiting.png',
-                    fit: BoxFit.cover,
-                  ),
+              ),
+              SizedBox(
+                height: 200,
+                child: Image.asset(
+                  'images/waiting.png',
+                  fit: BoxFit.cover,
                 ),
-              ],
-            )
-          : ListView.builder(
-              itemCount: transactions.length,
-              itemBuilder: (ctx, index) {
-                final tr = transactions[index];
-                return Card(
-                  margin:
-                      const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
-                  elevation: 5,
-                  child: ListTile(
-                    trailing: IconButton(
-                      onPressed: () => onRemove(tr.id),
-                      icon: const Icon(Icons.delete),
-                      color: Colors.red,
-                    ),
-                    title: Text(
-                      tr.title,
-                      style: Theme.of(context).textTheme.titleLarge,
-                    ),
-                    subtitle: Text(
-                      DateFormat('dd MMM yyyy').format(tr.date),
-                    ),
-                    leading: CircleAvatar(
-                      radius: 30,
-                      child: Padding(
-                        padding: const EdgeInsets.all(6.0),
-                        child: FittedBox(
-                          child: Text('R\$ ${tr.value.toStringAsFixed(2)}'),
-                        ),
+              ),
+            ],
+          )
+        : ListView.builder(
+            itemCount: transactions.length,
+            itemBuilder: (ctx, index) {
+              final tr = transactions[index];
+              return Card(
+                margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                elevation: 5,
+                child: ListTile(
+                  trailing: IconButton(
+                    onPressed: () => onRemove(tr.id),
+                    icon: const Icon(Icons.delete),
+                    color: Colors.red,
+                  ),
+                  title: Text(
+                    tr.title,
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                  subtitle: Text(
+                    DateFormat('dd MMM yyyy').format(tr.date),
+                  ),
+                  leading: CircleAvatar(
+                    radius: 30,
+                    child: Padding(
+                      padding: const EdgeInsets.all(6.0),
+                      child: FittedBox(
+                        child: Text('R\$ ${tr.value.toStringAsFixed(2)}'),
                       ),
                     ),
                   ),
-                );
-              },
-            ),
-    );
+                ),
+              );
+            },
+          );
   }
 }
