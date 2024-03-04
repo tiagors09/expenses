fb12192742adb6a24be115c4e3ba89014f3b4f14
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-03-04 09:16:47 -0300

    added adaptative_date_picker.dart

---
+++ lib/components/adaptative_date_picker.dart
@@ -0,0 +1,64 @@
+import 'dart:io';
+import 'package:intl/intl.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AdaptativeDatePicker extends StatelessWidget {
+  final DateTime selectedDate;
+  final Function(DateTime) onDateChange;
+
+  const AdaptativeDatePicker({
+    super.key,
+    required this.selectedDate,
+    required this.onDateChange,
+  });
+
+  _showDatePicker(BuildContext context) {
+    showDatePicker(
+      context: context,
+      firstDate: DateTime(1999),
+      lastDate: DateTime.now(),
+      initialDate: DateTime.now(),
+    ).then((pickedDate) {
+      onDateChange(pickedDate!);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Platform.isIOS
+        ? SizedBox(
+            height: 180,
+            child: CupertinoDatePicker(
+              mode: CupertinoDatePickerMode.date,
+              initialDateTime: DateTime.now(),
+              minimumDate: DateTime(1999),
+              maximumDate: DateTime.now(),
+              onDateTimeChanged: onDateChange,
+            ),
+          )
+        : SizedBox(
+            height: 70,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    selectedDate == DateTime(0, 0, 0)
+                        ? 'Nenhuma data selecionada'
+                        : 'Data selecionada ${DateFormat('dd/MM/y').format(selectedDate)}',
+                  ),
+                ),
+                TextButton(
+                  onPressed: () => _showDatePicker(context),
+                  child: const Text(
+                    'Selecionar Data',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          );
+  }
+}
