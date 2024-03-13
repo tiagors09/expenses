8e782ce9f8dc4b7594bd54fca085102fc3003665
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-03-13 09:41:53 -0300

    transaction_list.dart: refactored code using Transcation Item component, created in other file

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -1,7 +1,6 @@
+import 'package:expenses/components/transaction_item.dart';
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:intl/intl.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
@@ -42,45 +41,7 @@ class TransactionList extends StatelessWidget {
             itemCount: transactions.length,
             itemBuilder: (ctx, index) {
               final tr = transactions[index];
-              return Card(
-                margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
-                elevation: 5,
-                child: ListTile(
-                  trailing: MediaQuery.of(ctx).size.width > 480
-                      ? IconButton(
-                          onPressed: () => onRemove(tr.id),
-                          icon: const Icon(Icons.delete),
-                          color: Colors.red,
-                        )
-                      : TextButton.icon(
-                          onPressed: () => onRemove(tr.id),
-                          icon: const Icon(
-                            Icons.delete,
-                            color: Colors.red,
-                          ),
-                          label: const Text(
-                            'Excluir',
-                            style: TextStyle(color: Colors.red),
-                          ),
-                        ),
-                  title: Text(
-                    tr.title,
-                    style: Theme.of(context).textTheme.titleLarge,
-                  ),
-                  subtitle: Text(
-                    DateFormat('dd MMM yyyy').format(tr.date),
-                  ),
-                  leading: CircleAvatar(
-                    radius: 30,
-                    child: Padding(
-                      padding: const EdgeInsets.all(6.0),
-                      child: FittedBox(
-                        child: Text('R\$ ${tr.value.toStringAsFixed(2)}'),
-                      ),
-                    ),
-                  ),
-                ),
-              );
+              return TransactionItem(onRemove: onRemove, tr: tr);
             },
           );
   }
